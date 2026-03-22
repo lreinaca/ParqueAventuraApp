363dd25117a384463b0d883a594e1ad6ddaab3f8
LorenaReina <lorenareina2016@gmail.com>
2026-03-21 19:07:37 -0500

    configuracion de dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "com.eam.parqueaventuraapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.eam.parqueaventuraapp"
@@ -42,12 +38,18 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    // UI - Jetpack Compose & Material 3
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // Navegación
+    implementation(libs.androidx.navigation.compose)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
